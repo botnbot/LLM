978dc54549ea0f3dc27e6615ee17050c42731f73
Sergey Korobeynikov <skoro@smn35.com>
2026-03-11 18:05:45 +0300

    User model added to users.models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from users.managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    phone = models.CharField(max_length=11, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
