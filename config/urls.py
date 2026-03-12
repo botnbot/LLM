c7eb09630f16843cd965567042f1cbf32993dcf5
Sergey Korobeynikov <skoro@smn35.com>
2026-03-12 18:21:13 +0300

    ull in urlpattern for courses changed for materials

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("courses/", include('materials.urls',namespace='materials')),
+    path("materials/", include("materials.urls", namespace="materials")),
 ]
 
 if settings.DEBUG:
