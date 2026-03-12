6d33a1c667053e10013a2327701d2273bbc9ce24
Sergey Korobeynikov <skoro@smn35.com>
2026-03-12 18:26:04 +0300

    APIViews imported to materials/urls.py urlpatterns to APIViews added

--- materials/urls.py
+++ materials/urls.py
@@ -1,12 +1,33 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
-from rest_framework.urls import app_name
+
+from materials.views import (
+    LessonViewSet,
+    CourseCreateAPIView,
+    CourseRetrieveAPIView,
+    CourseListAPIView,
+    CourseUpdateAPIView,
+    CourseDestroyAPIView,
+)
 from materials.apps import MaterialsConfig
 
-from materials.views import CourseViewSet
-app_name = 'materials'
+app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register('',CourseViewSet, basename='courses')
+router.register("", LessonViewSet, basename="lessons")
 
-urlpatterns = []
+urlpatterns = [
+    path("courses/", CourseListAPIView.as_view(), name="courses_list"),
+    path("courses/<int:pk>/", CourseRetrieveAPIView.as_view(), name="courses_retrieve"),
+    path("courses/create/", CourseCreateAPIView.as_view(), name="courses_create"),
+    path(
+        "courses/<int:pk>/delete/",
+        CourseDestroyAPIView.as_view(),
+        name="courses_delete",
+    ),
+    path(
+        "courses/<int:pk>/update/", CourseUpdateAPIView.as_view(), name="courses_update"
+    ),
+]
+
 urlpatterns += router.urls
