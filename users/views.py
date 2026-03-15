f1970eefbadf9d8f6af69173d29ef09ccbe14d08
Sergey Korobeynikov <skoro@smn35.com>
2026-03-16 02:45:41 +0300

    PaymentsListAPIView added

--- users/views.py
+++ users/views.py
@@ -3,7 +3,25 @@ from rest_framework.viewsets import ModelViewSet
 from materials.models import Course
 from materials.serializers import CourseSerializer
 
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
+from users.models import Payments
+from users.serializers import PaymentsSerializer
+
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class PaymentsListAPIView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+
+    filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
+
+    ordering_fields = ["payment_date"]
