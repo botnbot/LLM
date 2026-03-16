ad4a9a3c4ce6eac2771ffdb287786955df362de3
Sergey Korobeynikov <skoro@smn35.com>
2026-03-17 02:41:14 +0300

    urlpatterns for register&login added in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from users.views import PaymentsListAPIView
 
-app_name = "users"
+from users.apps import UsersConfig
+from users.views import PaymentsListAPIView, UserCreateAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+app_name = UsersConfig.name
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
-    ]
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
