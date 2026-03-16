56b868e42d9c92aa62ad663d2bfd8598239c08cc
Sergey Korobeynikov <skoro@smn35.com>
2026-03-17 02:42:00 +0300

    UserCreateAPIView created

--- users/views.py
+++ users/views.py
@@ -1,15 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
-
-from materials.models import Course
-from materials.serializers import CourseSerializer
-
-from rest_framework.generics import ListAPIView
+from click import password_option
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-from users.models import Payments
+from users.models import Payments, User
+from users.serialaizers import UserSerializer
 from users.serializers import PaymentsSerializer
-
 
 
 class PaymentsListAPIView(ListAPIView):
@@ -21,3 +18,15 @@ class PaymentsListAPIView(ListAPIView):
     filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
 
     ordering_fields = ["payment_date"]
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active =True)
+        user.set_password(user.password)
+        user.save()
