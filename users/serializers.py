5d553d43ce34cedc5cbc83319c8a955d73a2ddcd
Sergey Korobeynikov <skoro@smn35.com>
2026-03-17 12:33:42 +0300

    UserUserSerializer created PaymentsSerializer modified

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework.serializers import ModelSerializer
-from users.models import Payments
+
+from materials.serializers import CourseSerializer, LessonSerializer
+from users.models import User, Payments
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
 
 
 class PaymentsSerializer(ModelSerializer):
+    paid_course = CourseSerializer(read_only=True)
+    paid_lesson = LessonSerializer(read_only=True)
 
     class Meta:
         model = Payments
         fields = "__all__"
+
+    @staticmethod
+    def get_paid_item(obj):
+        if obj.paid_course:
+            return obj.paid_course.name
+        if obj.paid_lesson:
+            return obj.paid_lesson.name
+        return None
