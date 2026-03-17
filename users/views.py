452d88dbdd94b397b1ba80d6daa6530c4f13afdf
Sergey Korobeynikov <skoro@smn35.com>
2026-03-17 12:38:29 +0300

    CRUD users APIViews created PaymentsListAPIView created

--- users/views.py
+++ users/views.py
@@ -2,23 +2,35 @@ from click import password_option
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import AllowAny
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import Payments, User
-from users.serialaizers import UserSerializer
+from users.serializers import UserSerializer
 from users.serializers import PaymentsSerializer
 
+#  CRUD пользователей
 
-class PaymentsListAPIView(ListAPIView):
-    queryset = Payments.objects.all()
-    serializer_class = PaymentsSerializer
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
 
-    filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    ordering_fields = ["payment_date"]
 
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+#  Регистрация пользователя
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
@@ -27,6 +39,17 @@ class UserCreateAPIView(CreateAPIView):
     authentication_classes = []
 
     def perform_create(self, serializer):
-        user = serializer.save(is_active =True)
-        user.set_password(user.password)
+        user = serializer.save(is_active=True)
+        user.set_password(serializer.validated_data["password"])
         user.save()
+
+
+# Платежи
+
+class PaymentsListAPIView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
+    ordering_fields = ["payment_date"]
