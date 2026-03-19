8bd9daa98d961913ec1a2da87128d1b9854e8c5c
Sergey Korobeynikov <skoro@smn35.com>
2026-03-19 10:17:36 +0300

    owner field added to Course&Lesson classes

--- users/management/commands/create_payments.py
+++ users/management/commands/create_payments.py
@@ -1,7 +1,9 @@
-from django.core.management.base import BaseCommand
 from datetime import datetime
-from users.models import User, Payments
+
+from django.core.management.base import BaseCommand
+
 from materials.models import Course, Lesson
+from users.models import Payments, User
 
 
 class Command(BaseCommand):
