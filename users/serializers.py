50938ab14c1d2aab75c66e9169da8525967468d4
Sergey Korobeynikov <skoro@smn35.com>
2026-03-21 03:04:30 +0300

    UserSerializer&UserProfileSerializer created

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework.serializers import ModelSerializer
-from users.models import Payments
+
+from users.models import Payments, User
+
+
+class UserSerializer(ModelSerializer):
+    """Полный сериализатор (для создания/редактирования)"""
+
+    class Meta:
+        model = User
+        exclude = ["is_staff", "is_superuser", "groups", "user_permissions"]
+
+
+class UserProfileSerializer(ModelSerializer):
+    """Ограниченный сериализатор (для просмотра профиля)"""
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "avatar", "phone", "city"]
 
 
 class PaymentsSerializer(ModelSerializer):
