75497c22d493e1c5172b05556b6be667c9b43f23
Sergey Korobeynikov <skoro@smn35.com>
2026-03-21 03:20:57 +0300

    UserViewSet created

--- users/views.py
+++ users/views.py
@@ -1,14 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
-
-from materials.models import Course
-from materials.serializers import CourseSerializer
-
-from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-from users.models import Payments
-from users.serializers import PaymentsSerializer
+from users.models import Payments, User
+from users.permissions import IsSelf
+from users.serializers import (PaymentsSerializer, UserProfileSerializer,
+                               UserSerializer)
 
 
 class PaymentsListAPIView(ListAPIView):
@@ -20,3 +19,20 @@ class PaymentsListAPIView(ListAPIView):
     filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
 
     ordering_fields = ["payment_date"]
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserProfileSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsSelf()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
