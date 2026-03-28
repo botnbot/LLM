e4c8b938fc4606f5ba9bec838736ed4d733fc797
Sergey Korobeynikov <skoro@smn35.com>
2026-03-28 14:57:51 +0300

    video_link field added to LessonSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
+
 from materials.models import Course, Lesson
+from materials.validators import validate_youtube_link
 from users.models import Subscription
 
 
@@ -11,9 +13,15 @@ class LessonSerializer(ModelSerializer):
         model = Lesson
         fields = "__all__"
 
+    video_link = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        validators=[validate_youtube_link]
+    )
+
 
 class CourseSerializer(ModelSerializer):
-
+    lessons = LessonSerializer(many=True, read_only=True)
     lessons_count = SerializerMethodField()
     is_subscribed = SerializerMethodField()
 
@@ -24,29 +32,20 @@ class CourseSerializer(ModelSerializer):
     def get_lessons_count(self, course):
         return course.lessons.count()
 
-    def get_is_subscribed(self, course):
-        user = self.context.get("request").user
-
-        if user.is_anonymous:
+    def get_is_subscribed(self, obj):
+        request = self.context.get("request")
+        if not request or request.user.is_anonymous:
             return False
-
-        return Subscription.objects.filter(user=user, course=course).exists()
+        return Subscription.objects.filter(user=request.user, course=obj).exists()
 
 
 class LessonDetailSerializer(ModelSerializer):
     count_course_lessons = SerializerMethodField()
-    course = serializers.PrimaryKeyRelatedField(read_only=True)
+    course = CourseSerializer()
 
     def get_count_course_lessons(self, lesson):
-        return lesson.course.lessons.count()
+        return Lesson.objects.filter(course=lesson.course).count()
 
     class Meta:
         model = Lesson
-        fields = [
-            "id",
-            "name",
-            "course",
-            "description",
-            "video_link",
-            "count_course_lessons",
-        ]
+        fields = ["name", "course", "description", "count_course_lessons"]
