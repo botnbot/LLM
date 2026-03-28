79b51ffaf2b292fe44dae1da57814f1b784ee767
Sergey Korobeynikov <skoro@smn35.com>
2026-03-28 15:34:16 +0300

    UserListAPIView, UserRetrieveAPIView created permission_classes & def get_queryset added to PaymentsListAPIView

--- users/views.py
+++ users/views.py
@@ -1,23 +1,86 @@
-from rest_framework.viewsets import ModelViewSet
-
-from materials.models import Course
-from materials.serializers import CourseSerializer
-
-from rest_framework.generics import ListAPIView
+from django.contrib.auth.hashers import make_password
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.models import Payments
-from users.serializers import PaymentsSerializer
+from users.models import Payments, User
+from users.permissions import IsOwner
+from users.serializers import PaymentsSerializer, UserSerializer, MyTokenObtainPairSerializer
+
+
+#  CRUD пользователей
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+
+#  Регистрация пользователя
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save(
+            password=make_password(serializer.validated_data["password"]),
+            is_active=True
+        )
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# Платежи
 
 
 class PaymentsListAPIView(ListAPIView):
-    queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Payments.objects.filter(user=self.request.user)
 
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-
     filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
-
     ordering_fields = ["payment_date"]
-    ordering = ["-payment_date"]
+
+
