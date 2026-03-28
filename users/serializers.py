cea587992a2a2097a63e017040a68afd65fc7e7f
Sergey Korobeynikov <skoro@smn35.com>
2026-03-28 15:00:26 +0300

    UserSerializer, PaymentsSerializer, MyTokenObtainPairSerializer added

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,41 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from users.models import Payments
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+from users.models import Payments, User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "password", "avatar", "phone", "city")
+        extra_kwargs = {"password": {"write_only": True}}
 
 
 class PaymentsSerializer(ModelSerializer):
+    paid_course = CourseSerializer(read_only=True)
+    paid_lesson = LessonSerializer(read_only=True)
+
+    paid_course_id = serializers.PrimaryKeyRelatedField(
+        queryset=Course.objects.all(),
+        source="paid_course",
+        write_only=True,
+        required=False,
+    )
+
+    paid_lesson_id = serializers.PrimaryKeyRelatedField(
+        queryset=Lesson.objects.all(),
+        source="paid_lesson",
+        write_only=True,
+        required=False,
+    )
 
     class Meta:
         model = Payments
         fields = "__all__"
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = "email"
