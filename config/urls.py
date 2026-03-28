eee7a1f79ac97368073429baab70f181250601f0
Sergey Korobeynikov <skoro@smn35.com>
2026-03-28 15:07:58 +0300

    materials path fix

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("materials/", include(("materials.urls", "materials"), namespace="materials")),
+    path("materials/", include("materials.urls", namespace="materials")),
     path("users/", include("users.urls", namespace="users")),
 ]
 
