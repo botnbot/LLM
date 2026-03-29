2c38cb0320640f4747fd55d2376cce8f09fb99b3
Sergey Korobeynikov <skoro@smn35.com>
2026-03-29 22:47:54 +0300

    urlpattern for PaymentsListAPIView double deleted

--- users/urls.py
+++ users/urls.py
@@ -32,7 +32,5 @@ urlpatterns = [
     path("users/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="users_delete"),
     #  Платежи
     path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
-    # path("payments/", PaymentsListAPICreateView.as_view(), name="payments_list"),
-
-    path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
+    # path("payments/", PaymentsCreateAPIView.as_view(), name="payments_list"),
 ]
