1c53b581d1272aafe1945c2fd6d93aa761ca7b28
Sergey Korobeynikov <skoro@smn35.com>
2026-04-02 17:49:45 +0300

    celery installed

--- config/settings.py
+++ config/settings.py
@@ -148,3 +148,27 @@ SWAGGER_SETTINGS = {
 }
 
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
+    }
+}
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379' 
+
+# URL-адрес брокера результатов
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
